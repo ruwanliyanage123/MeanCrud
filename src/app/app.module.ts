a966e491bc77762cc607594a0a6b4a03c81ae269
ruwanliyanage <ruwanliyanagegalle1994@gmail.com>
2019-05-14 19:50:12 +0530

    add create component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { EditComponent } from './components/edit/edit.component';
+import { CreateComponent } from './components/create/create.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     EditComponent,
+    CreateComponent,
   ],
   imports: [
     BrowserModule,
