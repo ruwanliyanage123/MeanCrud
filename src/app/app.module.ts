d4f70573a3b14edda6cdd638cbe87ef573b316e5
ruwanliyanage <ruwanliyanagegalle1994@gmail.com>
2019-05-14 19:54:43 +0530

    add list component and make routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,19 +5,12 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { EditComponent } from './components/edit/edit.component';
 import { CreateComponent } from './components/create/create.component';
-
+import { ListComponent } from './components/list/list.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    EditComponent,
-    CreateComponent,
-  ],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule
-  ],
+  declarations: [AppComponent, EditComponent, CreateComponent, ListComponent],
+  imports: [BrowserModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
