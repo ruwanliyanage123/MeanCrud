e44d76bb85733b31344320055adba000f4f26a05
ruwanliyanage <ruwanliyanagegalle1994@gmail.com>
2019-05-14 19:48:12 +0530

    add edit component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { EditComponent } from './components/edit/edit.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EditComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
