ea4dddb13e944684362d733e4092b23b2da879df
ruwanliyanage <ruwanliyanagegalle1994@gmail.com>
2019-05-15 18:45:28 +0530

    add funcitons into list component

--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { IssueService } from 'src/app/services/issue.service';
+import { Issue } from 'src/app/issue.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-list',
@@ -7,7 +9,30 @@ import { IssueService } from 'src/app/services/issue.service';
   styleUrls: ['./list.component.css']
 })
 export class ListComponent implements OnInit {
-  constructor(private issueService: IssueService) {}
+  constructor(private issueService: IssueService, private router: Router) {}
 
-  ngOnInit() {}
+  issues: Issue[];
+  displayedColumns = ['title', 'responsible', 'severity', 'status', 'actions'];
+
+  ngOnInit() {
+    this.fetchIssues();
+  }
+
+  fetchIssues() {
+    this.issueService.getIssues().subscribe((data: Issue[]) => {
+      this.issues = data;
+      console.log('data requested...');
+      console.log(this.issues);
+    });
+  }
+
+  editIssue(id) {
+    this.router.navigate([`/edit/${id}`]);
+  }
+
+  deleteIssue(id) {
+    this.issueService.deleteIssue(id).subscribe(() => {
+      this.fetchIssues();
+    });
+  }
 }
