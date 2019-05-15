c3c53d7904f7b8ad835b41ee1e5eb0f3b42ec75f
ruwanliyanage <ruwanliyanagegalle1994@gmail.com>
2019-05-15 17:51:05 +0530

    add function for add data

--- src/app/services/issue.service.ts
+++ src/app/services/issue.service.ts
@@ -8,11 +8,31 @@ export class IssueService {
   uri = 'http://localhost:3000';
   constructor(private http: HttpClient) {}
 
+  /**
+   * this funciton used for get the all issues
+   */
   getIssues() {
     return this.http.get(`${this.uri}/issues`);
   }
 
+  /**
+   * this function is used for the get the result relavent to the id number
+   */
   getIssuesById(id) {
     return this.http.get(`${this.uri}/issues/${id}`);
+  }
+
+  /**
+   * this funciton used for the add an issue
+   */
+
+  addIssue(title, responsible, description, severity) {
+    const issue = {
+      title: title,
+      responsible: responsible,
+      description: description,
+      severity: severity
+    };
+    return this.http.post(`${this.uri}/issues/add`, issue);
   }
 }
