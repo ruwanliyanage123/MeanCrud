68dbd5c56e5841187a7edb2c32e44e2c5d27a6ef
ruwanliyanage <ruwanliyanagegalle1994@gmail.com>
2019-05-15 20:11:26 +0530

    add edit component

--- src/app/components/edit/edit.component.ts
+++ src/app/components/edit/edit.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { IssueService } from 'src/app/services/issue.service';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router, ActivatedRoute } from '@angular/router';
+import { MatSnackBar } from '@angular/material';
 
 @Component({
   selector: 'app-edit',
@@ -7,7 +10,51 @@ import { IssueService } from 'src/app/services/issue.service';
   styleUrls: ['./edit.component.css']
 })
 export class EditComponent implements OnInit {
-  constructor(private issueService: IssueService) {}
+  id: String;
+  issue: any = {};
+  updateForm: FormGroup;
 
-  ngOnInit() {}
+  constructor(
+    private issueService: IssueService,
+    private router: Router,
+    private route: ActivatedRoute,
+    private snackBar: MatSnackBar,
+    private fb: FormBuilder
+  ) {
+    this.createForm();
+  }
+
+  ngOnInit() {
+    this.route.params.subscribe(params => {
+      this.id = params.id;
+      this.issueService.getIssuesById(this.id).subscribe(res => {
+        this.issue = res;
+        this.updateForm.get('title').setValue(this.issue.title);
+        this.updateForm.get('responsible').setValue(this.issue.title);
+        this.updateForm.get('description').setValue(this.issue.title);
+        this.updateForm.get('severity').setValue(this.issue.title);
+        this.updateForm.get('status').setValue(this.issue.title);
+      });
+    });
+  }
+
+  createForm() {
+    this.updateForm = this.fb.group({
+      title: ['', Validators.required],
+      responsible: '',
+      description: '',
+      severity: '',
+      status: ''
+    });
+  }
+
+  updateIssue(title, responsible, description, severity, status) {
+    this.issueService
+      .updateIssue(this.id, title, responsible, description, severity)
+      .subscribe(() => {
+        this.snackBar.open('issue update', 'ok', {
+          duration: 3000
+        });
+      });
+  }
 }
